e8ad2e62138203a119dc0b1f3414b3c0a5e0a249
Eli Sagy <sagyeli@gmail.com>
2016-01-10 03:04:01 +0200

    Raw links tooltip with a stub message

--- content.js
+++ content.js
@@ -1,3 +1,26 @@
 (function() {
-	console.log('Hello World!');	
+	$('body').tooltip({
+		content: function() {
+			return 'Some info...';
+		},
+		items: 'a',
+		open: function(event, ui) {
+			if (typeof(event.originalEvent) === 'undefined') {
+				return false;
+			}
+			var $id = $(ui.tooltip).attr('id');
+			$('div.ui-tooltip').not('#' + $id).remove();
+
+			// ajax function to pull in data and add it to the tooltip goes here
+		},
+		close: function(event, ui) {
+			ui.tooltip.hover(function() {
+				$(this).stop(true).fadeTo(400, 1); 
+			}, function() {
+				$(this).fadeOut('400', function() {
+					$(this).remove();
+				});
+			});
+		}
+	});
 })();
