1019e3ec8cdf04e2103dbe7bb273cd5ea235f5db
Eli Sagy <sagyeli@gmail.com>
2016-02-16 12:01:00 +0200

    Enabling like and dislike buttons

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 (function() {
 	var notifications = [],
-		removePanelTimeout;
+		removePanelTimeout,
+		currentHoveredHref = '';
 
 	socket.syncUpdates('notification', notifications, function(event, item, array) {
 		console.log('Notifications array is updated! Now it looks like this:');
@@ -15,12 +16,15 @@
 				offsetParent = $(this).offsetParent(),
 				isPositioned = offsetParent[0] !== $('html')[0];
 
+			currentHoveredHref = this.href;
+
 			$('pagebubblewrapper').attr('style', 'display: block; left: ' + (isPositioned || offset.left + offsetParent.left - 32 < 0 ? e.pageX - 64 : offset.left - 32) + 'px; top: ' + (isPositioned ? e.pageY - 12 : offset.top) + 'px;');
 
 			clearTimeout(removePanelTimeout);
 			removePanelTimeout = setTimeout(function() {
+				currentHoveredHref = '';
 				$('pagebubblewrapper').removeAttr('style');
-			}, 500);
+			}, 2000);
 		},
 		function(e) {
 			
@@ -32,7 +36,45 @@
 			clearTimeout(removePanelTimeout);
 		},
 		function(e) {
+			currentHoveredHref = '';
 			$('pagebubblewrapper').removeAttr('style');
 		}
 	);
+
+	$('pagebubblelikebtn').click(function() {
+		$.ajax({
+			type: "POST",
+			contentType: 'application/json',
+			dataType: 'json',
+			url: "http://pagebubble.com/api/notifications",
+			data: JSON.stringify({
+				name: 'like',
+				data: {
+					targetUrl: currentHoveredHref
+				},
+				originUrl: window.location.href
+
+			}),
+			success: function (response) {},
+			error: function (xhr, ajaxOptions, thrownError) {}
+		});
+	});
+
+	$('pagebubbledislikebtn').click(function() {
+		$.ajax({
+			type: "POST",
+			contentType: 'application/json',
+			dataType: 'json',
+			url: "http://pagebubble.com/api/notifications",
+			data: JSON.stringify({
+				name: 'dislike',
+				data: {
+					targetUrl: currentHoveredHref
+				},
+				originUrl: window.location.href
+			}),
+			success: function (response) {},
+			error: function (xhr, ajaxOptions, thrownError) {}
+		});
+	});
 })();
